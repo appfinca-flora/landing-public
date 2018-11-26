79457c06cbf407f8d9df09d43e974d53d21342fb
brandonshwu <brandon.shwu@gmail.com>
2018-11-26 09:18:38 +0800

    Google Tag Manager and FAQ/Promo routing.

--- js/l10n.js
+++ js/l10n.js
@@ -2,6 +2,7 @@
 var language = navigator.language || navigator.browserLanguage; // for IE
 if (language.toLowerCase().indexOf('zh-tw') > -1) {
   if (document.location.href.indexOf('zh-tw') < 0) {
-    document.location.href = '/zh-tw/index.html?' + window.location.search.substring(1);
+    // document.location.href = '/zh-tw/index.html?' + location.search.substring(1);
+    document.location.href = '/zh-tw' + location.pathname + location.search.substring(0);
   }
 }
