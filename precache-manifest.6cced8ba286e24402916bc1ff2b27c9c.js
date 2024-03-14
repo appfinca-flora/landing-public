b5ca4b8c6c00fa60304fa04847b8d59ac4f1aaf8
shwu <shwu@shwu-mbp24.local>
2024-03-14 16:17:25 +0800

    Tip for linear Garden.

---
+++ precache-manifest.6cced8ba286e24402916bc1ff2b27c9c.js
@@ -0,0 +1,370 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd685b16937d40e414fc51c4d2ed0626",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d290bd99fb7d7612e454",
+    "url": "/static/css/10.09fd38fa.chunk.css"
+  },
+  {
+    "revision": "a44be57a8534a8e6cef0",
+    "url": "/static/css/11.e0578a87.chunk.css"
+  },
+  {
+    "revision": "f4b33eaf7435d0f5f470",
+    "url": "/static/css/12.46d3d986.chunk.css"
+  },
+  {
+    "revision": "d27f3a2c42911d8c7bf7",
+    "url": "/static/css/13.996f9975.chunk.css"
+  },
+  {
+    "revision": "a69cc434f25021fe0ca0",
+    "url": "/static/css/14.edacad3e.chunk.css"
+  },
+  {
+    "revision": "ea7963b0dea91acb27be",
+    "url": "/static/css/15.fa9678d2.chunk.css"
+  },
+  {
+    "revision": "63e7c0f45e03be295c27",
+    "url": "/static/css/16.b7cada33.chunk.css"
+  },
+  {
+    "revision": "3f5717996d2a62953369",
+    "url": "/static/css/17.517f2e82.chunk.css"
+  },
+  {
+    "revision": "5c129b2ac1ee357ea5b0",
+    "url": "/static/css/4.4707e12a.chunk.css"
+  },
+  {
+    "revision": "2a888a5b3bad9b912e81",
+    "url": "/static/css/5.d572b08c.chunk.css"
+  },
+  {
+    "revision": "0f0553efb9c5d3fd8f4e",
+    "url": "/static/css/6.a7c83c63.chunk.css"
+  },
+  {
+    "revision": "514a18f0d7f1697183a4",
+    "url": "/static/css/7.891be337.chunk.css"
+  },
+  {
+    "revision": "540d096e4691dae45c2c",
+    "url": "/static/css/8.293af944.chunk.css"
+  },
+  {
+    "revision": "1a36a91e79ec9347d24e",
+    "url": "/static/css/9.bd1fed3d.chunk.css"
+  },
+  {
+    "revision": "74f231d3a4e49c98a49e",
+    "url": "/static/css/main.07c12846.chunk.css"
+  },
+  {
+    "revision": "5fc8e26eb0e9c0ecde4b",
+    "url": "/static/js/0.3bd153c8.chunk.js"
+  },
+  {
+    "revision": "d290bd99fb7d7612e454",
+    "url": "/static/js/10.87ad0100.chunk.js"
+  },
+  {
+    "revision": "a44be57a8534a8e6cef0",
+    "url": "/static/js/11.4b42f9e2.chunk.js"
+  },
+  {
+    "revision": "f4b33eaf7435d0f5f470",
+    "url": "/static/js/12.fd9c3102.chunk.js"
+  },
+  {
+    "revision": "d27f3a2c42911d8c7bf7",
+    "url": "/static/js/13.0b85fbfd.chunk.js"
+  },
+  {
+    "revision": "a69cc434f25021fe0ca0",
+    "url": "/static/js/14.eee763ad.chunk.js"
+  },
+  {
+    "revision": "ea7963b0dea91acb27be",
+    "url": "/static/js/15.dc7ae56e.chunk.js"
+  },
+  {
+    "revision": "63e7c0f45e03be295c27",
+    "url": "/static/js/16.5069bfee.chunk.js"
+  },
+  {
+    "revision": "3f5717996d2a62953369",
+    "url": "/static/js/17.814c893d.chunk.js"
+  },
+  {
+    "revision": "513f162c92262857b519",
+    "url": "/static/js/18.aa9d260f.chunk.js"
+  },
+  {
+    "revision": "dfdadffefa09d1383f46",
+    "url": "/static/js/19.76705a49.chunk.js"
+  },
+  {
+    "revision": "f1d2db3530fd03bfe88b",
+    "url": "/static/js/3.b057a996.chunk.js"
+  },
+  {
+    "revision": "35f1a3ca561e93b64bcba705f76bd30f",
+    "url": "/static/js/3.b057a996.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c129b2ac1ee357ea5b0",
+    "url": "/static/js/4.a4a785e5.chunk.js"
+  },
+  {
+    "revision": "2a888a5b3bad9b912e81",
+    "url": "/static/js/5.584fb6d9.chunk.js"
+  },
+  {
+    "revision": "0f0553efb9c5d3fd8f4e",
+    "url": "/static/js/6.d11fef0d.chunk.js"
+  },
+  {
+    "revision": "514a18f0d7f1697183a4",
+    "url": "/static/js/7.c183a062.chunk.js"
+  },
+  {
+    "revision": "540d096e4691dae45c2c",
+    "url": "/static/js/8.9afd43e9.chunk.js"
+  },
+  {
+    "revision": "1a36a91e79ec9347d24e",
+    "url": "/static/js/9.5d7cc3e3.chunk.js"
+  },
+  {
+    "revision": "74f231d3a4e49c98a49e",
+    "url": "/static/js/main.e73e3adc.chunk.js"
+  },
+  {
+    "revision": "d0be42ee3bc4d3136dbe",
+    "url": "/static/js/runtime-main.277841a3.js"
+  },
+  {
+    "revision": "ba2f74aa9229b853c701df66422950e8",
+    "url": "/static/media/app-access.ba2f74aa.svg"
+  },
+  {
+    "revision": "cab2eac60acd879c5b5d0b3c70ec913b",
+    "url": "/static/media/au.cab2eac6.svg"
+  },
+  {
+    "revision": "002ef7fc25c49e271d4629b164bd32a1",
+    "url": "/static/media/banner.002ef7fc.jpg"
+  },
+  {
+    "revision": "382eef52f4274edfb7d122bd5dbf40ff",
+    "url": "/static/media/banner.382eef52.png"
+  },
+  {
+    "revision": "409971ba8b470cba13aa30d57e2f88f9",
+    "url": "/static/media/banner.409971ba.jpg"
+  },
+  {
+    "revision": "7cf15687ad034183f76d145aabe74877",
+    "url": "/static/media/banner.7cf15687.jpg"
+  },
+  {
+    "revision": "939e8aebb6b720d6cbe96c186f66c06b",
+    "url": "/static/media/banner.939e8aeb.jpg"
+  },
+  {
+    "revision": "989b72f21d2b17c8ef032e39a21daa96",
+    "url": "/static/media/banner.989b72f2.jpg"
+  },
+  {
+    "revision": "fa0500fab4cc1aa231538b9fb137420c",
+    "url": "/static/media/banner.fa0500fa.jpg"
+  },
+  {
+    "revision": "f86666012fa93cee681e2a74d6d9290d",
+    "url": "/static/media/beautify-color.f8666601.svg"
+  },
+  {
+    "revision": "9e23e0a65db4474490109be03dcfbce6",
+    "url": "/static/media/beautify-diversity.9e23e0a6.svg"
+  },
+  {
+    "revision": "c387ce12d9b329fc11b546b5ef678536",
+    "url": "/static/media/beautify-unique.c387ce12.svg"
+  },
+  {
+    "revision": "a9e8bb8fd705d7e8982028e629a9929a",
+    "url": "/static/media/big-garden.a9e8bb8f.svg"
+  },
+  {
+    "revision": "ed3cd4b507f8209412bc426332f23589",
+    "url": "/static/media/ca.ed3cd4b5.svg"
+  },
+  {
+    "revision": "3a8e64332f7259d975705bf9b47ed462",
+    "url": "/static/media/care.3a8e6433.svg"
+  },
+  {
+    "revision": "de58f020c33242f67a37d657a87d176b",
+    "url": "/static/media/cta.de58f020.svg"
+  },
+  {
+    "revision": "8f36d1a50df1f76d9b4fff6bbe3513bc",
+    "url": "/static/media/farmer1.8f36d1a5.jpg"
+  },
+  {
+    "revision": "b88cecbc891787498a6923b0e6363dba",
+    "url": "/static/media/farmer1.b88cecbc.jpg"
+  },
+  {
+    "revision": "29bfda72a8a7c1b4f69bf399d2492f60",
+    "url": "/static/media/farmer2.29bfda72.jpg"
+  },
+  {
+    "revision": "7e76cf7e2a4bd6b594c725c0d4dacb6f",
+    "url": "/static/media/farmer2.7e76cf7e.jpg"
+  },
+  {
+    "revision": "5b24cb389d44083a850273290f23bc75",
+    "url": "/static/media/footer.5b24cb38.jpg"
+  },
+  {
+    "revision": "c6cd2a0b48b221eb18cbfd3e10b9bfd1",
+    "url": "/static/media/friends.c6cd2a0b.png"
+  },
+  {
+    "revision": "e1fc81c46b1c96bf8f15f197c0faaeb2",
+    "url": "/static/media/garden.e1fc81c4.png"
+  },
+  {
+    "revision": "e5564902e2642c5e6e2e98e68a7d41f5",
+    "url": "/static/media/gb.e5564902.svg"
+  },
+  {
+    "revision": "8e85058ed91520aa8a01aeacd7606f3a",
+    "url": "/static/media/gears.8e85058e.svg"
+  },
+  {
+    "revision": "6449a88b13858279bd70cde241415ae9",
+    "url": "/static/media/habit.6449a88b.png"
+  },
+  {
+    "revision": "8d7dbe097ea4be8ff3d77052745ff1ca",
+    "url": "/static/media/header-h.8d7dbe09.jpg"
+  },
+  {
+    "revision": "71381f691d415831a6ffa0201cc41553",
+    "url": "/static/media/header-tail.71381f69.svg"
+  },
+  {
+    "revision": "73d3b13c1e002e3af803e79749f359e9",
+    "url": "/static/media/header-v.73d3b13c.jpg"
+  },
+  {
+    "revision": "098a0292e01f3033478e5e767aa9eb21",
+    "url": "/static/media/icon-font.098a0292.woff2"
+  },
+  {
+    "revision": "b0e7ce7d16686bcf59b8177625768d79",
+    "url": "/static/media/icon-font.b0e7ce7d.woff"
+  },
+  {
+    "revision": "e1fcdcfb879d18e5a7a5a0f700a778cb",
+    "url": "/static/media/icon-font.e1fcdcfb.eot"
+  },
+  {
+    "revision": "2223553f20b0063c42e28e17e8fd8f1b",
+    "url": "/static/media/icon.2223553f.svg"
+  },
+  {
+    "revision": "54a1d0f1d4688187b921c2822f5e7796",
+    "url": "/static/media/kill.54a1d0f1.png"
+  },
+  {
+    "revision": "6449a88b13858279bd70cde241415ae9",
+    "url": "/static/media/linear-garden.6449a88b.png"
+  },
+  {
+    "revision": "9e5477855410bef88d7076f6c9f440e7",
+    "url": "/static/media/nz.9e547785.svg"
+  },
+  {
+    "revision": "d26ad7ecb163aebdfe6925c541b91157",
+    "url": "/static/media/partner.d26ad7ec.png"
+  },
+  {
+    "revision": "7ed666783493f6f9fac595cf9bb9d0ee",
+    "url": "/static/media/plant.7ed66678.png"
+  },
+  {
+    "revision": "052da94e33424449a98a1aa24dc0b41c",
+    "url": "/static/media/preview.052da94e.jpg"
+  },
+  {
+    "revision": "70a54713a116f0b33150abdd645e4879",
+    "url": "/static/media/preview.70a54713.png"
+  },
+  {
+    "revision": "7886a2888f22d3ca6e4f90e8f00c2761",
+    "url": "/static/media/preview.7886a288.jpg"
+  },
+  {
+    "revision": "8f37534aa0bf516e4b74bf0265b98fcd",
+    "url": "/static/media/preview.8f37534a.jpg"
+  },
+  {
+    "revision": "ec2a90c30bf884325e3b9a72176c0dc2",
+    "url": "/static/media/preview.ec2a90c3.jpg"
+  },
+  {
+    "revision": "030ef39b317f2f52f8dedb19cef44ae7",
+    "url": "/static/media/price.030ef39b.svg"
+  },
+  {
+    "revision": "08ca0770c113dac17a15b57e4c88ef97",
+    "url": "/static/media/real-tree-2.08ca0770.jpg"
+  },
+  {
+    "revision": "eb6d4ad0abc94d24b0ceab36a5a6a9a6",
+    "url": "/static/media/timer.eb6d4ad0.svg"
+  },
+  {
+    "revision": "33abe205d860862d332af07919c9c1c1",
+    "url": "/static/media/todo.33abe205.png"
+  },
+  {
+    "revision": "1668d854ed6cfdf578da0c3b0e46b35a",
+    "url": "/static/media/tour.1668d854.png"
+  },
+  {
+    "revision": "09607352ecf88a3b5fbfc94242a17fdf",
+    "url": "/static/media/train.09607352.jpg"
+  },
+  {
+    "revision": "36b391f6f72b4e64bb2c2c61282eab41",
+    "url": "/static/media/train.36b391f6.jpg"
+  },
+  {
+    "revision": "3912449d447934737d8213b37d0d7553",
+    "url": "/static/media/train.3912449d.jpg"
+  },
+  {
+    "revision": "e2db790ee102abc2f29abb71fb2b87d4",
+    "url": "/static/media/tw.e2db790e.svg"
+  },
+  {
+    "revision": "36ab476e5e55f496749ee61897a9cfb5",
+    "url": "/static/media/us.36ab476e.svg"
+  },
+  {
+    "revision": "ea20fdcc8d95a69947d3eb8ecc8c72b6",
+    "url": "/static/media/widget.ea20fdcc.png"
+  },
+  {
+    "revision": "0aba8cdc74a06712f90208da37e1db46",
+    "url": "/static/media/world.0aba8cdc.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d867fc0343e1765dc5c49104277ad0e5.js"
+  "/precache-manifest.6cced8ba286e24402916bc1ff2b27c9c.js"
 );
 
 self.addEventListener('message', (event) => {
